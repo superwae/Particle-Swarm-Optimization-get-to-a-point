dd114b36cffe0bbcf0c648acc02e26dda374840d
wael salameh <90796176+superwae@users.noreply.github.com>
2023-02-02 13:37:16 +0200

    Add files via upload

---
+++ PSO.py
@@ -0,0 +1,81 @@
+import random
+import math 
+
+
+max_iter=1000
+target_location=[69,69]
+N=100
+minx=-50
+maxx=50
+w=1
+c1=0.8
+c2=0.8
+bestfit=1000
+best_sworm_postionx=0
+best_sworm_postiony=0
+sworm=[]
+bool=False
+def fintness(x,y):
+    x2=[x,y]
+    fit=((math.dist(x2, target_location)))
+    return fit
+
+class particle:
+  def __init__(self, postionx,postiony,volictyx,volictyy):
+    self.postionx = postionx
+    self.postiony = postiony
+    self.bestpostionx = postionx
+    self.bestpostiony = postiony
+    self.volictyx=volictyx
+    self.volictyy=volictyy
+
+  def testfit(self,postionx,postiony):
+    if(fintness(postionx,postiony)<fintness(self.bestpostionx,self.bestpostiony)):
+        self.bestpostionx=postionx
+        self.bestpostiony=postiony
+
+
+
+for i in range (N):
+    x=random.randint(minx,maxx)
+    y=random.randint(minx,maxx)
+    volictyx=random.randint(minx,maxx)
+    volictyy=random.randint(minx,maxx)
+    postion={x,y}
+    if bool==False:
+        best_sworm_postionx=x
+        best_sworm_postiony=y
+        bestfit=fintness(x,y)
+        bool=True
+    fit=fintness(x,y)
+    if(fit<bestfit):
+        bestfit=fit
+        best_sworm_postionx=x
+        best_sworm_postiony=y
+    sworm.append(particle(x,y,volictyx,volictyy))
+
+for i in range (max_iter):
+    for particles in sworm:
+        particles.volictyx=(w*particles.volictyx)+c1*(particles.bestpostionx-particles.postionx)+c2*(best_sworm_postionx-particles.postionx)
+        particles.volictyy=(w*particles.volictyy)+c1*(particles.bestpostiony-particles.postiony)+c2*(best_sworm_postionx-particles.postiony)
+        if( particles.volictyx>maxx):
+             particles.volictyx=maxx
+        elif(particles.volictyx<minx):
+             particles.volictyx=minx
+        elif(particles.volictyy<minx):
+             particles.volictyy=minx
+        elif(particles.volictyy<minx):
+             particles.volictyy=minx
+        particles.postionx=particles.postionx+particles.volictyx
+        particles.postiony=particles.postiony+particles.volictyy
+
+        if(fintness(particles.postionx,particles.postiony)<bestfit):
+            bestfit=fintness(particles.postionx,particles.postiony)
+            best_sworm_postionx=particles.postionx
+            best_sworm_postiony=particles.postiony
+
+        if(fintness(particles.postionx,particles.postiony)<fintness(particles.bestpostionx,particles.bestpostiony)):
+           particles.bestpostionx =particles.postionx
+           particles.bestpostiony =particles.postiony
+print(bestfit)
+print(best_sworm_postionx,best_sworm_postiony)
